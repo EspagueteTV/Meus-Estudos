482e41bbb142ac927a05fb66680f64d1c280bdfb
Gabriel Siqueira <espaguetetv.sbs@gmail.com>
2020-10-02 20:15:07 -0300

    Organização do Repositório

--- Java/MaratonaJava/src/br/com/siqueira/javacore/introducaoclasses/classes/Estudante.java
+++
@@ -1,8 +0,0 @@
-package br.com.siqueira.javacore.introducaoclasses.classes;
-
-public class Estudante {        // Molde, para diversos objetos, de diversos estudantes
-    public String nome;             // public: modificador de acesso
-    public String matricula;
-    public int idade;
-
-}
--- Java/MaratonaJava/src/br/com/siqueira/javacore/introducaometodos/teste/EstudanteTeste2.java
+++
@@ -1,7 +0,0 @@
-package br.com.siqueira.javacore.introducaometodos.teste;
-
-public class EstudanteTeste2 {
-    public static void main(String[] args) {
-
-    }
-}
